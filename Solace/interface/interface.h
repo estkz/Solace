a7290199b613d33e9a997b22aa0ef99c6cafc313
chxnisa <chanisa.visser@gmail.com>
2023-08-28 01:04:58 +0200

    init

--- Solace/interface/interface.h
+++ Solace/interface/interface.h
@@ -1,5 +1,8 @@
 #pragma once
 
+// We create this namespace with all the functions used for setting up the imgui window
+// The declared functions and variables down below can be accessed using the scope resolution operator '::'
+// Example of usage: gui::
 namespace gui
 {
     // The width and height of the menu (in pixels)
